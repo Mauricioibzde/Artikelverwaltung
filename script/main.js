5efd814af801c075ec46bfbaf2016a72d8d02ae9
Mauricio Farias da Silva <m.fariasdasilva@hotmail.com>
2025-09-22 03:07:05 +0200

    melhorando as funcionalidades

--- script/main.js
+++ script/main.js
@@ -1,3 +1,7 @@
 import { readSelector } from "./modules/Universal-selector.js";
 import { addTask } from "./modules/Function-add-task.js";
 import { openDescriptionTask } from "./modules/open-task.js";
+
+
+
+
